7e566fb9654700597cdec9dd94026afae6e4c5c3
merryround <merryround@cresoty.co.kr>
2025-06-05 10:57:54 +0900

    기능 추가

--- script.js
+++ script.js
@@ -43,8 +43,9 @@ let gameOver = true;
 
 class Piece {
     constructor(shape = null, color = null) {
-        this.shape = shape || SHAPES[Math.floor(Math.random() * SHAPES.length)];
-        this.color = color || COLORS[SHAPES.findIndex(s => s === this.shape)];
+        const randomIndex = Math.floor(Math.random() * SHAPES.length);
+        this.shape = shape || SHAPES[randomIndex];
+        this.color = color || COLORS[randomIndex];
         this.x = Math.floor((COLS - this.shape[0].length) / 2);
         this.y = 0;
     }
@@ -62,12 +63,31 @@ class Piece {
             });
         });
     }
+
+    hardDrop() {
+        while (!this.isCollision(0, 1)) {
+            this.y++;
+        }
+    }
+
+    isCollision(moveX = 0, moveY = 0) {
+        return this.shape.some((row, y) => {
+            return row.some((value, x) => {
+                if (!value) return false;
+                const newX = this.x + x + moveX;
+                const newY = this.y + y + moveY;
+                return newX < 0 || newX >= COLS || 
+                       newY >= ROWS || 
+                       (newY >= 0 && board[newY][newX]);
+            });
+        });
+    }
 }
 
 function createNewPiece() {
     currentPiece = nextPiece || new Piece();
     nextPiece = new Piece();
-    if (isCollision()) {
+    if (currentPiece.isCollision(0, 0)) {
         gameOver = true;
         clearInterval(gameLoop);
         alert('게임 오버! 점수: ' + score);
@@ -77,6 +97,21 @@ function createNewPiece() {
 function drawBoard() {
     ctx.fillStyle = '#fff';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    // 그리드 라인 그리기
+    ctx.strokeStyle = '#ddd';
+    for (let i = 0; i <= ROWS; i++) {
+        ctx.beginPath();
+        ctx.moveTo(0, i * BLOCK_SIZE);
+        ctx.lineTo(canvas.width, i * BLOCK_SIZE);
+        ctx.stroke();
+    }
+    for (let i = 0; i <= COLS; i++) {
+        ctx.beginPath();
+        ctx.moveTo(i * BLOCK_SIZE, 0);
+        ctx.lineTo(i * BLOCK_SIZE, canvas.height);
+        ctx.stroke();
+    }
     
     board.forEach((row, y) => {
         row.forEach((value, x) => {
@@ -93,24 +128,30 @@ function drawNextPiece() {
     nextPieceCtx.fillStyle = '#fff';
     nextPieceCtx.fillRect(0, 0, nextPieceCanvas.width, nextPieceCanvas.height);
     
-    if (nextPiece) {
-        const offsetX = Math.floor((5 - nextPiece.shape[0].length) / 2);
-        const offsetY = Math.floor((5 - nextPiece.shape.length) / 2);
-        nextPiece.draw(nextPieceCtx, offsetX, offsetY);
+    // 그리드 라인 그리기
+    nextPieceCtx.strokeStyle = '#ddd';
+    for (let i = 0; i <= 5; i++) {
+        nextPieceCtx.beginPath();
+        nextPieceCtx.moveTo(0, i * BLOCK_SIZE);
+        nextPieceCtx.lineTo(nextPieceCanvas.width, i * BLOCK_SIZE);
+        nextPieceCtx.stroke();
     }
-}
-
-function isCollision() {
-    return currentPiece.shape.some((row, y) => {
-        return row.some((value, x) => {
-            if (!value) return false;
-            const newX = currentPiece.x + x;
-            const newY = currentPiece.y + y;
-            return newX < 0 || newX >= COLS || 
-                   newY >= ROWS || 
-                   (newY >= 0 && board[newY][newX]);
-        });
-    });
+    for (let i = 0; i <= 5; i++) {
+        nextPieceCtx.beginPath();
+        nextPieceCtx.moveTo(i * BLOCK_SIZE, 0);
+        nextPieceCtx.lineTo(i * BLOCK_SIZE, nextPieceCanvas.height);
+        nextPieceCtx.stroke();
+    }
+    
+    if (nextPiece) {
+        const tempX = nextPiece.x;
+        const tempY = nextPiece.y;
+        nextPiece.x = 1;
+        nextPiece.y = 1;
+        nextPiece.draw(nextPieceCtx);
+        nextPiece.x = tempX;
+        nextPiece.y = tempY;
+    }
 }
 
 function rotate() {
@@ -119,7 +160,7 @@ function rotate() {
     );
     const originalShape = currentPiece.shape;
     currentPiece.shape = newShape;
-    if (isCollision()) {
+    if (currentPiece.isCollision(0, 0)) {
         currentPiece.shape = originalShape;
     }
 }
@@ -157,9 +198,9 @@ function clearLines() {
 }
 
 function drop() {
-    currentPiece.y++;
-    if (isCollision()) {
-        currentPiece.y--;
+    if (!currentPiece.isCollision(0, 1)) {
+        currentPiece.y++;
+    } else {
         merge();
         clearLines();
         createNewPiece();
@@ -167,9 +208,8 @@ function drop() {
 }
 
 function move(dir) {
-    currentPiece.x += dir;
-    if (isCollision()) {
-        currentPiece.x -= dir;
+    if (!currentPiece.isCollision(dir, 0)) {
+        currentPiece.x += dir;
     }
 }
 
@@ -224,6 +264,12 @@ document.addEventListener('keydown', event => {
             break;
         case 'ArrowUp':
             rotate();
+            break;
+        case ' ':  // 스페이스바
+            currentPiece.hardDrop();
+            merge();
+            clearLines();
+            createNewPiece();
             break;
     }
 });
